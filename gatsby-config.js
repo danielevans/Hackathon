1a8fb56fa400f1c4ee3a113899ece34a4a9d1626
Daniel Evans <evans.daniel.n@gmail.com>
2018-11-03 15:20:10 -0700

    Fix json syntax error

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,13 +10,7 @@ module.exports = {
       options: {
         name: `images`,
         path: `${__dirname}/src/images`,
-    },
-    {
-      resolve: `gatsby-plugin-typography`,
-      options: {
-        pathToConfigModule: `src/utils/typography.js`,
       },
-    },
     },
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
