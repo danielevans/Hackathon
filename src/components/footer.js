67bd1d2fdd22e35fd4a6cd515f96cc0a6cb8fcb4
Anton G Neuhold Jr <agneuhold@gmail.com>
2018-11-03 16:27:36 -0700

    Moved CSS files to folder Moded the CSS files and changed their respective locations in the code to reflect that change. 😁

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import { Link } from 'gatsby'
-import './components.css'
+import './css/footer.css'
 
 const Footer = () => (
-  <div className="headerFooter">
+  <div className="footer">
     <div className="footerContent">
       <p style={{ margin: 0 }}>
         © 2018, PDX Hackathon Group with Peter, Daniel1, DanielA, Aaron, and Tony!
