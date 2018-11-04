176d585648befe36eb7c21b241298e0564ad9a0d
Daniel Ramsayer <13422934+danielramsayer@users.noreply.github.com>
2018-11-04 09:53:55 -0800

    Minor updates to the following components.

--- src/components/returnResult.js
+++ src/components/returnResult.js
@@ -1,10 +1,25 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Helmet from 'react-helmet'
+import React, { Component } from 'react'
 import {StaticQuery, graphql} from 'gatsby'
 import {Link} from 'gatsby'
-import Header from './header'
-import ImageUpload from './ImageUpload'
-import Footer from './footer'
 import './css/layout.css'
 import { Card, CardImg, CardTitle, CardText, CardBody, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+
+function Results(props) {
+  
+}
+
+
+  function renderResults ({result}) {
+    const entryResult = result.map((eachResult) => {
+      return (
+        <div>
+          <div> {this.state.weighted} <div/>
+          <div> {this.state.Author} <div/>
+          <div> {this.state.Work} <div/>
+        </div>)
+    });
+    return entryResult;
+  }
+}
+
+export default returnResult;
