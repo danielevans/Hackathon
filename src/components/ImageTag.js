6b7bb5f560fab36d49117a880f0ac1a7e8f58196
Peter Durham <peterdurham@protonmail.com>
2018-11-04 11:08:23 -0800

    added tag selection

--- src/components/ImageTag.js
+++ src/components/ImageTag.js
@@ -2,9 +2,24 @@ import React from 'react';
 import './css/imageUpload.css';
 
 const ImageTag = (props)=>{
-    return(
-        <div className="image__tag">
+    let selected = props.selectedTags;
+    
+    let index = selected.indexOf(props.tagName);
+    console.log("index", index);
+
+    let selectedDisplay = (index>-1) ?
+    <div className="image__tag image__tag--selected" onClick={()=>props.selectTag(props.tagName)}>
             {props.tagName}
+    </div>: null
+
+    let unselectedDisplay = (index<0) ?
+    <div className="image__tag" onClick={()=>props.selectTag(props.tagName)}>
+            {props.tagName}
+    </div>: null
+
+    return(
+        <div>
+            {selectedDisplay}{unselectedDisplay}
         </div>
     );
 }
--- src/components/ImageUpload.js
+++ src/components/ImageUpload.js
@@ -14,12 +14,22 @@ const app = new Clarifai.App({
 
 
 class ImageUpload extends React.Component {
-    state = {
-        tags: [],
-        urlInput: "",
-        url: "",
-        files: []
+    constructor(props) {
+        super(props);
+        this.selectTagHandler = this.selectTagHandler.bind(this);
+
+        this.state = {
+            tags: [],
+            selectedTags: [],
+            urlInput: "",
+            url: "",
+            imageName: "",
+            files: [],
+            uploadDisplay: true
+        }
     }
+    
+  
     
     
     bindInput = (input) => {
@@ -28,7 +38,7 @@ class ImageUpload extends React.Component {
     selectURL = () => {
         let imageURL = this.state.urlInput.toString();
         
-        this.setState({url: imageURL})
+        this.setState({url: imageURL, imageName: imageURL, selectedTags: []})
         this.linkAPIHandler(imageURL);
     }
     
@@ -43,7 +53,7 @@ class ImageUpload extends React.Component {
             console.log("concepts", concepts);
             const imageTags = concepts.map((tag)=>tag.name);
             console.log('link array', imageTags);
-            this.setState({tags: imageTags})
+            this.setState({tags: imageTags, uploadDisplay: false})
         })
     }
 
@@ -63,9 +73,9 @@ class ImageUpload extends React.Component {
             (response) => {
                 var concepts = response['outputs'][0]['data']['concepts']
                 const imageTags = concepts.map((tag)=>tag.name);
-                console.log('uploadarray', imageTags);
+                console.log('file', file);
               
-              this.setState({tags: imageTags, url: file.base64})
+              this.setState({tags: imageTags, url: file.base64, imageName: file.name, uploadDisplay: false, selectedTags: []})
             }
             // ,
             // function(err) {
@@ -73,38 +83,74 @@ class ImageUpload extends React.Component {
             // }
           );
     }
-    
-    
+    displayUpload = () => {
+        this.setState({uploadDisplay: true})
+    }
+
+    selectTagHandler = (tagName) => {
+        let selected = [...this.state.selectedTags];
+        console.log(selected);
+        if(selected.indexOf(tagName)<0){
+            console.log('push tag', tagName);
+            selected.push(tagName);
+            this.setState({selectedTags: selected});
+        } else{
+            let index = selected.indexOf(tagName)
+            console.log('splice tag', index);
+            selected.splice(index, 1);
+            this.setState({selectedTags: selected});
+        }
+    }
 
     render() {
-        return(
-            <div>
+        let uploader = (this.state.uploadDisplay === true) ?
+        <div>
                 <div className="upload__options">
-                    <div className="upload__url">
-                        <div>Enter Image URL</div>
-                        <input type="text" onChange={(e)=>this.bindInput(e.target.value)}/>
-                        <button onClick={()=>this.selectURL()}>Select URL</button>
-                    </div>
                     <div className="upload__link">
-                        
                         <FileBase64
                             multiple={ true }
                             onDone={ this.getFiles.bind(this) } />
-
                     </div>
+                    <div className="upload__text">OR</div>
+                    <div className="upload__url">
+                        <input placeholder="Enter Image URL" className="upload__input" type="text" onChange={(e)=>this.bindInput(e.target.value)}/>
+                        <button className="upload__button" onClick={()=>this.selectURL()}>Select URL</button>
+                    </div>
+                    
                 </div>
+                
+            </div> : null
+
+        let changeUpload = (this.state.uploadDisplay === false) ?
+        <div className="upload__change">
+            <div className="upload__current">Current Upload:{this.state.imageName}</div>
+            <button onClick={()=>this.displayUpload()}>Upload new photo</button> 
+        </div>: null
+
+        let refineSearch = (this.state.selectedTags.length > 4) ?
+        <button className="upload__search">Search selected tags</button> : null
+
+
+        return(
+            <div>
+                {uploader}{changeUpload}
                 <div className="image__display">
                     <div className="image__div">
-                    <img className="image" src={this.state.url}/>
+                        <img className="image" src={this.state.url}/>
                     </div>
                     
                     <div className="image__tags">
-                        {this.state.tags.map((tag)=><ImageTag 
+                        {this.state.tags.map((tag)=><ImageTag
+                            key={tag} 
+                            selectTag = {this.selectTagHandler}
                             tagName = {tag}
+                            selectedTags = {this.state.selectedTags}
                         />)}
+                       <div className="image_refine">{refineSearch}</div>
                     </div>
                     
                 </div>
+                
             </div>
         );
     }
